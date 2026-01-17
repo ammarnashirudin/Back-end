0a409181198e9e99482f09737f9424b1617471f4
ammarnashirudin <ammarnashirudin14@gmail.com>
2026-01-17 08:37:13 +0700

    addNodemailer

--- property-renting-web-app-be/src/configs/env.configs.ts
+++ property-renting-web-app-be/src/configs/env.configs.ts
@@ -4,11 +4,15 @@ const PORT = process.env.PORT || 8000;
 const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || "";
 const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET || "";
 const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME || "";
+const GMAIL_EMAIL = process.env.GMAIL_EMAIL || "";
+const GMAIL_APP_PASS = process.env.GMAIL_APP_PASS || "";
+
 
 export {
     PORT,
     CLOUDINARY_API_KEY,
     CLOUDINARY_API_SECRET,
     CLOUDINARY_CLOUD_NAME,
-    
-}
+    GMAIL_EMAIL,
+    GMAIL_APP_PASS,
+};
