f776879191ff09691ad087f83c325325545770bb
ammarnashirudin <ammarnashirudin14@gmail.com>
2026-01-24 12:43:29 +0700

    restart

--- property-renting-web-app-be/src/main.ts
+++ property-renting-web-app-be/src/main.ts
@@ -1,16 +1,8 @@
 import express from "express";
-import errorMiddleware from "./middlewares/error.middlewares.js";
+
 import cors from "cors";
 import helmet from "helmet";
-import PropertyRouter from "./routers/property.routers.js";
-import roomsRouter from "./routers/room.routers.js";
-import availabilityRouter from "./routers/availability.controller.js";
-import peakSeasonRateRouter from "./routers/peakSeasonRate.routers.js";
-import authRouter from "./routers/auth.routers.js";
-import loginRouter from "./routers/login.routers.js";
-import verifyRouter from "./routers/verify.routers.js";
-import pofileRouter from "./routers/profile.routers.js";
-import tenantRouter from "./routers/tenant.routers.js";
+
 
 import { PORT } from "./configs/env.configs.js";
 
@@ -24,20 +16,11 @@ app.use(express.json());
 
 // Routes
 
-app.use("/auth", authRouter);
-app.use("/profile", pofileRouter);
-app.use("/verify", verifyRouter);
-app.use("/tenant",tenantRouter);
 
-app.use("/properties", PropertyRouter);
-app.use("/rooms", roomsRouter);
-app.use("/availabilityRoom", availabilityRouter);
-app.use("/peakSeasonRate", peakSeasonRateRouter);
-app.use("/login", loginRouter);
 
 
 //error Middlewares
-app.use(errorMiddleware);
+
 
 
 // Start the server
